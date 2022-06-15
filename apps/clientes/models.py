91de05a2cdead9c4b7ab475a80a0d7b25c9c62e8
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-14 22:45:11 -0300

    Implemented Cliente model

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from apps.core.models import Auditoria
+from django_cpf_cnpj.fields import CPFField, CNPJField
 
-# Create your models here.
+
+class Cliente(Auditoria):
+    nome = models.CharField(max_length=250, null=True, blank=True)
+    cpf = CPFField(masked=True, null=True, blank=True)
+    cnpj = CNPJField(masked=True, null=True, blank=True)
+
+    def __str__(self):
+        if self.cpf:
+            return self.cpf + " - " + self.nome
+        elif self.cnpj:
+            return self.cnpj + " - " + self.nome
+        else:
+            return self.nome

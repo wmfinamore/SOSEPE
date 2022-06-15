ab6ab8c921228f4ef12451533aee9af6760b1fa8
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-14 22:57:30 -0300

    Registered Cliente model in admin module

--- apps/clientes/admin.py
+++ apps/clientes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import Cliente
 
-# Register your models here.
+
+class ClienteAdmin(SimpleHistoryAdmin):
+    list_display = ['nome', 'cpf', 'cnpj']
+    search_fields = ['nome', 'cpf', 'cnpj']
+
+
+admin.site.register(Cliente, ClienteAdmin)

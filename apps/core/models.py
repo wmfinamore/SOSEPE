6820867d746dc7d6ec286fdbec1c99963f3c5e51
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-14 22:30:03 -0300

    Implemented Auditoria abstract class

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class Auditoria(models.Model):
+    data_inclusao = models.DateField(auto_now_add=True)
+    hora_inclusao = models.DateTimeField(auto_now_add=True)
+    data_alteracao = models.DateField(auto_now=True)
+    hora_alteracao = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
+
+    class Meta:
+        abstract = True

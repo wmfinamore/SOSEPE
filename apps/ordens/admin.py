90ee842499c7adddd9d85f7344917dfead023f52
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-16 23:09:16 -0300

    Registered OrdemServico model in admin module

--- apps/ordens/admin.py
+++ apps/ordens/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import OrdemServico
 
-# Register your models here.
+
+class OrdemServicoAdmin(SimpleHistoryAdmin):
+    list_display = ['numero', 'pedido', 'cliente', 'descricao', 'data_entrega']
+    search_fields = ['numero', 'pedido', 'cliente', 'descricao', 'data_entrega']
+    autocomplete_fields = ['cliente']
+
+
+admin.site.register(OrdemServico, OrdemServicoAdmin)

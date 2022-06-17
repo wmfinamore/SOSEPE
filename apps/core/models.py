5069317d9598cf555f4bdbd8151d36c3efa56518
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-16 22:54:44 -0300

    Defined verbose name for Auditoria fields

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from simple_history.models import HistoricalRecords
 
 
+# TODO: Definir Verbose Names
 class Auditoria(models.Model):
-    data_inclusao = models.DateField(auto_now_add=True)
-    hora_inclusao = models.DateTimeField(auto_now_add=True)
-    data_alteracao = models.DateField(auto_now=True)
-    hora_alteracao = models.DateTimeField(auto_now=True)
+    data_inclusao = models.DateField(auto_now_add=True, verbose_name='Data de Inclusão')
+    hora_inclusao = models.DateTimeField(auto_now_add=True, verbose_name='Hora de Inclusão')
+    data_alteracao = models.DateField(auto_now=True, verbose_name='Data de Alteração')
+    hora_alteracao = models.DateTimeField(auto_now=True, verbose_name='Hora de Alteração')
     history = HistoricalRecords(inherit=True)
 
     class Meta:

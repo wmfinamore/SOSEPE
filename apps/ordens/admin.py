9a6e45982bed28b1e067ed8d9d89c2bb5ce87812
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-17 21:03:32 -0300

    Registered StatusOrdemServicos model in admin module

--- apps/ordens/admin.py
+++ apps/ordens/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
-from .models import OrdemServico
+from .models import OrdemServico, StatusOrdemServico
 
 
 class OrdemServicoAdmin(SimpleHistoryAdmin):
@@ -10,3 +10,4 @@ class OrdemServicoAdmin(SimpleHistoryAdmin):
 
 
 admin.site.register(OrdemServico, OrdemServicoAdmin)
+admin.site.register(StatusOrdemServico)

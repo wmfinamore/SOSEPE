fe537f64d1cccd85833b43bf1dbb8e34bdeb4af6
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-18 12:51:58 -0300

    Defined colors for indicators in OrdemServicoForm

--- apps/ordens/forms.py
+++ apps/ordens/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import OrdemServico
 
 
 class OrdermServicoForm(forms.ModelForm):
@@ -6,3 +7,20 @@ class OrdermServicoForm(forms.ModelForm):
     data_entrega = forms.DateField(widget=forms.widgets.TextInput(attrs={'type': 'date'}), required=False)
     data_entrega_real = forms.DateField(widget=forms.widgets.TextInput(attrs={'type': 'date'}), required=False)
 
+    def __init__(self, *args, **kwargs):
+        super(OrdermServicoForm, self).__init__(*args, **kwargs)
+
+        if self.instance.id:
+            if self.instance.situacao == 'Entregue com Atraso':
+                self.fields["numero"].widget.attrs["style"] = 'background-color: yellow;'
+            elif self.instance.situacao == 'Entregue no Prazo':
+                self.fields["numero"].widget.attrs["style"] = 'background-color: green;'
+            elif self.instance.situacao == 'Pedido Atrasado':
+                self.fields["numero"].widget.attrs["style"] = 'background-color: tomato;'
+            elif self.instance.situacao == 'Em risco de Atraso':
+                self.fields["numero"].widget.attrs["style"] = 'background-color: orange;'
+            elif self.instance.situacao == 'Em andamento':
+                self.fields["numero"].widget.attrs["style"] = 'background-color: cyan;'
+
+        class Meta:
+            model: OrdemServico

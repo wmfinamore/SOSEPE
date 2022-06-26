1916e553fb1e598a2dc4274ba517b77ae6b9f041
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-06-25 22:25:01 -0300

    Configured login/logout routes and template

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 # Rota para ativar o debug toolbar quando do django estiver em modo debug
